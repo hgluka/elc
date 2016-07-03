69a7b79667e8d6ab6caf2b333b56ca927347354e
lucantrop <luka.hadzi@gmail.com>
2016-07-03 09:16:09 +0200

    finished parser

--- elc/elc.py
+++ elc/elc.py
@@ -5,15 +5,37 @@ from collections import namedtuple as nt
 class Env(object):
 
     def __init__(self):
-        self.builtins = {'+': [lambda x,y: x+y, 2]}
+        self.builtins = {'+': lambda x,y: x+y,
+                         'fun': self.fun}
+
+        self.defined = {}
+
+    def fun(self, *args): # not fully implemented (or tested) [TODO]
+        fun_name = args[0]
+        variables = []
+        body = []
+        i = 1
+        while (arg[i].__class__.__name__ == 'arg' and arg[i].arg_type == word):
+            variables.append(arg)
+            i += 1
+        if arg[i].__class__.__name__ == 'fun':
+            body = arg[i]
+
+        self.defined[fun_name] = (variables, body)
+
 
 class Parser(object):
+    '''
+    Used to parse list of lines to an abstract syntax tree,
+    in the form of a nested named tuple
+    '''
 
     def __init__(self):
         self.string = r'^"([^"]*)"'
         self.number = r'^\d+\b'
         self.word = r'^[^\s{},"]+'
         self.evaluator = r'^{.*}'
+        self.fun_tuple = nt('fun', 'operator arguments')
         self.arg_tuple = nt('arg', 'arg_type value')
         self.env = Env()
 
@@ -37,8 +59,8 @@ class Parser(object):
         if(func.arg_type != 'word'):
             return 'Syntax Error: Evaluators must start with words.'
         else:
-            return self.evaluate(func.value, [i for i in self.parse_args(eval[1::])]) # separate evaluation from parsing,
-                                                                                      # or find another way to make function defs possible
+            return self.fun_tuple(operator=func, arguments=[i for i in self.parse_args(eval[1::])]) # separate evaluation from parsing,
+                                                                                               # or find another way to make function defs possible
 
     def parse_args(self, arg_list):
         i=0
@@ -53,18 +75,19 @@ class Parser(object):
                 yield self.parse_arg(arg_list[i])
                 i += 1
 
+    ''' to be implemented separately
     def evaluate(self, func_name, args):
-        if (func_name not in self.env.builtins):
+        if (func_name not in self.env.builtins or func_name not in self.env.defined):
             return 'Evaluator Error: Evaluator not defined.'
-        func = self.env.builtins[func_name][0]
+        func = self.env.builtins[func_name]
         arg_vals = [i.value for i in args]
         try:
-            if (self.env.builtins[func_name][1] != len(arg_vals)):
-                return 'Evaluator Error: Wrong number of arguments given.'       # change error reporting system to be actually useful
             return self.parse_arg(str(func(*arg_vals)))
         except TypeError:
+            return 'Evaluator Error: Wrong number of arguments given.'       # change error reporting system to be actually useful and easier to maintain
+        except AttributeError:
             return 'Evaluator Error: Evaluator given wrong type of argument.'
-
+    '''
 
 if __name__=='__main__':
     print('elc v0.1')
--- tests/elc_tests.py
+++ tests/elc_tests.py
@@ -4,7 +4,8 @@ from elc.elc import *
 
 def test_parser():
     parser = Parser()
-    assert_equals(parser.parse_eval("{+ 3 5}"), parser.arg_tuple(arg_type='value', value=8))
+    assert_equals(parser.parse_eval("{+ 3 5}"), parser.fun_tuple(operator=parser.arg_tuple('word', '+'),
+                                                          arguments=[parser.arg_tuple('value', 3), parser.arg_tuple('value', 5)]))
 
 def test_env():
     pass
