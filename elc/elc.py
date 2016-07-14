d07758607e1c5a7172c0349a5500f8085a2c1245
lucantrop <luka.hadzi@gmail.com>
2016-07-14 14:00:15 +0200

    utilized recursive regex to parse whole programs

--- elc/elc.py
+++ elc/elc.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import re
+import regex
 from collections import namedtuple as nt
 
 class Env(object):
@@ -13,15 +13,16 @@ class Env(object):
     def fun(self, *args): # not fully implemented (or tested) [TODO]
         fun_name = args[0]
         variables = []
-        body = []
         i = 1
         while (arg[i].__class__.__name__ == 'arg' and arg[i].arg_type == word):
             variables.append(arg)
             i += 1
         if arg[i].__class__.__name__ == 'fun':
             body = arg[i]
+        else:
+            return 'Syntax Error: Function body missing'
 
-        self.defined[fun_name] = (variables, body)
+        return (fun_name, variables, body)
 
 
 class Parser(object):
@@ -34,33 +35,33 @@ class Parser(object):
         self.string = r'^"([^"]*)"'
         self.number = r'^\d+\b'
         self.word = r'^[^\s{},"]+'
-        self.evaluator = r'^{.*}'
+        self.evaluator = r'^{.*}' # edit to work with both nested and leveled evaluator expressions
+        self.block_tuple = nt('block', 'evals')
         self.fun_tuple = nt('fun', 'operator arguments')
         self.arg_tuple = nt('arg', 'arg_type value')
-        self.env = Env()
 
 
     def parse_arg(self, arg):
-        match = re.match(self.string, arg)
+        match = regex.match(self.string, arg)
         if (match):
-            return self.arg_tuple(arg_type='value', value=match.group(1))
-        match = re.match(self.number, arg)
+            return self.arg_tuple(arg_type='value', value=match[0])
+        match = regex.match(self.number, arg)
         if (match):
-            return self.arg_tuple(arg_type='value', value=int(match.group(0)))
-        match = re.match(self.word, arg)
+            return self.arg_tuple(arg_type='value', value=int(match[0]))
+        match = regex.match(self.word, arg)
         if (match):
-            return self.arg_tuple(arg_type='word', value=match.group(0))
+            return self.arg_tuple(arg_type='word', value=match[0])
 
     def parse_eval(self, stmt):
-        evaluator = re.match(self.evaluator, stmt)
-        eval = evaluator.group(0)[1:len(evaluator.group(0))-1]
-        eval = eval.split()
-        func = self.parse_arg(eval[0])
+        evaluator = regex.match(self.evaluator, stmt)
+        ev = evaluator[0][1::]
+        ev = ev.split()
+        print(ev)
+        func = self.parse_arg(ev[0])
         if(func.arg_type != 'word'):
             return 'Syntax Error: Evaluators must start with words.'
         else:
-            return self.fun_tuple(operator=func, arguments=[i for i in self.parse_args(eval[1::])]) # separate evaluation from parsing,
-                                                                                               # or find another way to make function defs possible
+            return self.fun_tuple(operator=func, arguments=[i for i in self.parse_args(ev[1::])])
 
     def parse_args(self, arg_list):
         i=0
@@ -75,19 +76,26 @@ class Parser(object):
                 yield self.parse_arg(arg_list[i])
                 i += 1
 
-    ''' to be implemented separately
-    def evaluate(self, func_name, args):
-        if (func_name not in self.env.builtins or func_name not in self.env.defined):
-            return 'Evaluator Error: Evaluator not defined.'
-        func = self.env.builtins[func_name]
-        arg_vals = [i.value for i in args]
-        try:
-            return self.parse_arg(str(func(*arg_vals)))
-        except TypeError:
-            return 'Evaluator Error: Wrong number of arguments given.'       # change error reporting system to be actually useful and easier to maintain
-        except AttributeError:
-            return 'Evaluator Error: Evaluator given wrong type of argument.'
-    '''
+    def parse_program(self, program):
+        eval_list = []
+        for match in regex.findall(r'{((?:[^{}]|(?R))*)}', program):
+            eval_list.append(self.parse_eval('{'+match+'}'))
+        return self.block_tuple(eval_list)
+
+
+''' to be implemented separately
+def evaluate(self, func_name, args):
+    if (func_name not in self.env.builtins or func_name not in self.env.defined):
+        return 'Evaluator Error: Evaluator not defined.'
+    func = self.env.builtins[func_name]
+    arg_vals = [i.value for i in args]
+    try:
+        return self.parse_arg(str(func(*arg_vals)))
+    except TypeError:
+        return 'Evaluator Error: Wrong number of arguments given.'       # change error reporting system to be actually useful and easier to maintain
+    except AttributeError:
+        return 'Evaluator Error: Evaluator given wrong type of argument.'
+'''
 
 if __name__=='__main__':
     print('elc v0.1')
@@ -97,4 +105,4 @@ if __name__=='__main__':
 
     while True:
         prog = input('> ')
-        print(parser.parse_eval(prog))
+        print(parser.parse_program(prog))
